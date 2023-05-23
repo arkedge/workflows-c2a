730de8d45a60405157ce92d2c4e1088d063e07ab
sksat <sksat@arkedgespace.com>
2023-05-24 01:53:58 +0900

    split C2A setup bat

---
+++ action-c2a-build-win32/c2a-setup.bat
@@ -0,0 +1,11 @@
+echo "Link C2A user dir to ./c2a_user"
+
+mklink /j /d ".\\c2a_user" ".\\repo\\"%0
+
+cd c2a_user
+
+echo "Check setup script exist"
+if exist setup.bat (
+  echo "Setup"
+  ./setup.bat
+)
